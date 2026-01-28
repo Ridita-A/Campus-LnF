19b068bfb4266e2f2e23902878307d5c143e5255
Saba Atharique <164325140+sabaatharique@users.noreply.github.com>
2026-01-29 00:01:59 +0600

    Revert "Found report added"

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const lostRoutes = require('./routes/lostReportRoutes');
-const foundRoutes = require('./routes/foundReportRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/lost', lostRoutes);
-app.use('/api/found', foundRoutes);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
