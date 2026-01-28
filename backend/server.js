9efc8f2a25669a34ed1884b7b146a6a8976fa4aa
Ridita-A <riditadyuti@gmail.com>
2026-01-29 00:40:50 +0600

    Found Report bug fixed

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const lostRoutes = require('./routes/lostReportRoutes');
+const foundRoutes = require('./routes/foundReportRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/lost', lostRoutes);
+app.use('/api/found', foundRoutes);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
--- database/procedures.sql
+++ database/procedures.sql
@@ -38,3 +38,44 @@ BEGIN
     END LOOP;
 END;
 $$;
+
+CREATE OR REPLACE PROCEDURE create_found_report(
+    p_creator_id INT,
+    p_found_location_id INT,
+    p_title VARCHAR(50),
+    p_description TEXT,
+    p_found_at TIMESTAMP,
+    p_tags INT[],
+    p_image_urls TEXT[]
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    new_found_id INT;
+    tag_id INT;
+    image_url TEXT;
+BEGIN
+    -- Insert main found report
+    INSERT INTO Found_Report (
+        creator_id, found_location_id, title, description, found_at, status
+    )
+    VALUES (
+        p_creator_id, p_found_location_id, p_title, p_description, p_found_at, 'active'
+    )
+    RETURNING found_id INTO new_found_id;
+
+    -- Insert tags
+    FOREACH tag_id IN ARRAY p_tags
+    LOOP
+        INSERT INTO Found_Report_Tags (found_id, category_id)
+        VALUES (new_found_id, tag_id);
+    END LOOP;
+
+    -- Insert images
+    FOREACH image_url IN ARRAY p_image_urls
+    LOOP
+        INSERT INTO Found_Report_Images (found_id, image_url)
+        VALUES (new_found_id, image_url);
+    END LOOP;
+END;
+$$
